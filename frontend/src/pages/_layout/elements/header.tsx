e4fed01dd7df67a9cec52668adfdf0733487188f
rahulk67 <rahul@MacBookPro.lan>
2026-04-15 11:32:53 +0530

    chnages

--- frontend/src/pages/_layout/elements/header.tsx
+++ frontend/src/pages/_layout/elements/header.tsx
@@ -203,8 +203,8 @@ const Header = () => {
           {/* Center - Info */}
           <div className="text-center">
             {/* @ts-ignore */}
-            <div className="fw-bold">Main PTI {balance.balance - (balance?.exposer + balance?.casinoexposer) }</div>
-            <div>
+<div className="fw-bold">Main PTI {Math.max(0, (balance?.balance || 0) - ((balance?.exposer || 0) + (balance?.casinoexposer || 0)))}
+</div>           <div>
              <a
                         href="#"
                         onClick={getExposer}
